4e0afae50a331edf341b117b4df92b5daa5fef07
Ajay Kumar <ajaykumar767580@gmail.com>
2026-06-22 20:17:02 +0530

    Using the Django Form Class

--- reviews/views.py
+++ reviews/views.py
@@ -2,21 +2,32 @@ from django.http import HttpResponseRedirect
 
 from django.shortcuts import render
 
+from .forms import ReviewForm
+
 # Create your views here.
 
 def review(request):
-    if request.method == "POST":
-        entered_username = request.POST["username"]
+    # if request.method == "POST":
+    #     entered_username = request.POST["username"]
         
-        if entered_username == "" and len(entered_username) >= 100:
-            return render(request, 'reviews/review.html', {
-                 "has_error": True             
-            })
-        print(entered_username)
-        return HttpResponseRedirect("/thank-you")
+    #     if entered_username == "" and len(entered_username) >= 100:
+    #         return render(request, 'reviews/review.html', {
+    #              "has_error": True             
+    #         })
+    #     print(entered_username)
+    #     return HttpResponseRedirect("/thank-you")
+    
+    if request.method == 'POST':
+        form = ReviewForm(request.POST)
+        
+        if form.is_valid():
+            print(form.cleaned_data)
+            return HttpResponseRedirect("/thank-you")
+    
+    form = ReviewForm()
         
     return render(request, 'reviews/review.html', {
-        "has_error": False
+        "form": form
     })
 
 def thank_you(request):
