4748535a1009bb3994f2d9a3d187a0f1450015f9
Ajay Kumar <ajaykumar767580@gmail.com>
2026-06-23 12:20:42 +0530

    Introducing ModelForm

--- reviews/forms.py
+++ reviews/forms.py
@@ -1,9 +1,18 @@
 from django import forms
 
-class ReviewForm(forms.Form):
-    user_name = forms.CharField(label="Your Name", max_length=100, error_messages={
-        "required": "Your name must not be empty",
-        "max_length": "You can't enter a name more than 100 characters"
-    })
-    review_text = forms.CharField(label="Your Feedback", widget=forms.Textarea, max_length=200)
-    rating = forms.IntegerField(label="Your Rating", min_value=1, max_value=5)
+from reviews.models import Review
+
+# class ReviewForm(forms.Form):
+#     user_name = forms.CharField(label="Your Name", max_length=100, error_messages={
+#         "required": "Your name must not be empty",
+#         "max_length": "You can't enter a name more than 100 characters"
+#     })
+#     review_text = forms.CharField(label="Your Feedback", widget=forms.Textarea, max_length=200)
+#     rating = forms.IntegerField(label="Your Rating", min_value=1, max_value=5)
+
+class ReviewForm(forms.ModelForm):
+    class Meta:
+        model = Review
+        fields = '__all__'
+        # exclude = ['owner_comment']
+         
