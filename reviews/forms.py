8349b3f83b3216341c6f6370f6e407a085dc8304
Ajay Kumar <ajaykumar767580@gmail.com>
2026-06-24 10:47:54 +0530

    Configuring the ModelForm

--- reviews/forms.py
+++ reviews/forms.py
@@ -15,4 +15,15 @@ class ReviewForm(forms.ModelForm):
         model = Review
         fields = '__all__'
         # exclude = ['owner_comment']
+        labels = {
+            "user_name": "Your Name",
+            "review_text": "Your Feedback",
+            "rating": "Your Rating"
+        }
+        error_messages = {
+            "user_name": {
+                "required": "Your name must not be empty!",
+                "max_length": "You can't enter a name more than 100 characters"
+            }
+        }
          
